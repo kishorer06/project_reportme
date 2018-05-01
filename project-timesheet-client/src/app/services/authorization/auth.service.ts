61f2413079f34eaf1f8321432f7fe24aafa9f25f
Kishore <kishorer666@gmail.com>
2018-04-30 21:33:17 -0400

    Added changes to remove footer

--- project-timesheet-client/src/app/services/authorization/auth.service.ts
+++ project-timesheet-client/src/app/services/authorization/auth.service.ts
@@ -4,18 +4,17 @@ import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { User } from '../../model/user';
 import 'rxjs/add/operator/map';
 import { AppComponent } from '../../app.component';
-import { ConfigurationService } from "../../services/configuration/configuration.service";
+import { ApiConstants } from 'assets/api.constants'
+import { environment } from 'environments/environment'
 import { Observable } from "rxjs/Observable";
 import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
 export class AuthService {
-  api;
-  tokenInfo;
-  constructor(public http: Http, private _configs: ConfigurationService) {
-    this.api = _configs;
+  apiUrl = environment.apiUrl;
+  constructor(public http: Http) {
   }
-  public logIn(user: User): Observable<boolean> {
+  logIn(user: User) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json')
     // creating base64 encoded String from user name and password
@@ -24,20 +23,20 @@ export class AuthService {
     let options = new RequestOptions();
     options.headers = headers;
 
-    return this.http.post(this.api.configs.token, JSON.stringify({ username: user.username, password: user.password }), options)
+    return this.http.post(this.apiUrl + ApiConstants.token, JSON.stringify({ username: user.username, password: user.password }), options)
       .map((response: Response) => {
         // login successful if there's a jwt token in the response
         let token = response.json() && response.json().token;
         if (token) {
           // store username and jwt token in local storage to keep user logged in between page refreshes
-          localStorage.setItem('ACCESSID', JSON.stringify({username: user.username, TID: token}));
+          localStorage.setItem('ACCESSID', JSON.stringify({ username: user.username, TID: token }));
           // return true to indicate successful login
           return true;
         } else {
           // return false to indicate failed login
           return false;
         }
-      }).catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+      });
   }
 
   getToken(): String {
--- project-timesheet-client/src/app/services/user/user.service.ts
+++ project-timesheet-client/src/app/services/user/user.service.ts
@@ -1,30 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ConfigurationService } from '../configuration/configuration.service';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { User } from "../../model/user";
 import { AppComponent } from '../../app.component';
 import { AuthService } from "app/services/authorization/auth.service";
 import { Observable } from "rxjs/Observable";
+import { ApiConstants } from 'assets/api.constants'
+import { environment } from 'environments/environment'
 
 
 @Injectable()
 export class UserService {
-  api;
+  apiUrl = environment.apiUrl;
   private headers = new Headers({
     'Content-Type': 'application/json',
     'Authorization': 'Bearer ' + this.authService.getToken()
   });
-  constructor(private http: Http, private authService: AuthService, private _configService: ConfigurationService) {
-    this.api = _configService;
+  constructor(private http: Http, private authService: AuthService) {
   }
 
   createAccount(user: User) {
-    return this.http.post(this.api.configs.register, user)
+    return this.http.post(this.apiUrl + ApiConstants.register, user)
       .map(resp => resp.json());
   }
 
-  getUserInfo(user: User){
-    return this.http.get(this.api.configs.getUser, {headers: this.headers, params : "email=" + user})
+  getUserInfo(user: User) {
+    return this.http.get(this.apiUrl + ApiConstants.getUser, { headers: this.headers, params: "email=" + user })
       .map(res => res.json());
   }
 }
