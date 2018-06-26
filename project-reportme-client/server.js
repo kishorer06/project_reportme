dbad7327045ba2ad6d6673003491c92868b05b76
Kishore <kishorer666@gmail.com>
2018-06-25 21:47:24 -0400

    Changes for heroku delploy

--- project-reportme-client/server.js
+++ project-reportme-client/server.js
@@ -1,5 +1,17 @@
-var express = require('express')
-var app = express()
 
-app.use(express.static(_dirname+'/dist'));
-app.listen(process.env.port || 3000);
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3000);
